71b04a5f4fe0fe3acfeabc047d56b484c09600bf
mfpankau <74994673+mfpankau@users.noreply.github.com>
2021-07-26 16:46:17 -0400

    Update Joystick.h

--- Joystick/Joystick.h
+++ Joystick/Joystick.h
@@ -5,17 +5,19 @@
 class Joystick
 {
     public:
-        struct jVals{
+        struct jData{
             float xVal;
             float yVal;
+            bool btnPressed;
         };
-        Joystick(int RxPin, int RyPin);
-        Joystick(int RxPin, int RyPin, int neutralRead);
-        jVals ReadVals();
+        Joystick(int RxPin, int RyPin, int BtnPin);
+        Joystick(int RxPin, int RyPin, int BtnPin, int neutralRead);
+        jData ReadVals();
         bool flipY = false;
     private:
         int _RxPin;
         int _RyPin;
+        int _BtnPin;
         int _neutralRead;
         float normalizeVal(float value);
 };
